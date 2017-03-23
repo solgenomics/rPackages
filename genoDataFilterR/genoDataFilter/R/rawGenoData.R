33d6c3b143e046892590fae672894563c66b4bd7
Isaak <iyt2@cornell.edu>
2017-03-23 16:24:22 +0100

    clean up

---
+++ genoDataFilterR/genoDataFilter/R/rawGenoData.R
@@ -0,0 +1,10 @@
+#' @title Sample genotype dataset
+#'
+#' @description A data.frame of genotype dataset for testing
+#' @name rawGenoData
+#' @docType data
+#' @format data.frame
+#' @usage rawGenoData
+#'
+#' @keywords datasets
+NULL
