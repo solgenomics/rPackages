6018ed43022d2191b9426cd68e5d1e8657e5a22b
Isaak <iyt2@cornell.edu>
2019-01-11 23:18:40 +0100

    add clean meta cols

---
+++ phenoAnalysis/R/cleanMetaCols.R
@@ -0,0 +1,38 @@
+#' Removes meta data columns from the phenotype data
+#'
+#' Given a dataset with the BrAPI style column naming, removes meta data columns from the phenotype data.
+#'
+#' @param metaData a vector of columns to remove.
+#' @param metaDataFile a file with the meta data columns, tab-delimited. Optional
+#' @param keepMetaCols a vector of meta columns to keep in the data set.
+#' @param phenoData a data frame of the phenotype dataset
+#' @param phenoFile a name of the file with the phenotype dataset
+#' @return a data frame of the phenotype dataset with the germplasmName, trait columns, and meta columns exempt from removal.
+#' @export
+#'
+cleanMetaCols <- function(metaDataFile=NULL, phenoData=NULL, phenoFile=NULL,metaData=NULL,keepMetaCols=NULL) {
+
+  if (is.null(metaData) & is.null(metaDataFile)) {
+  stop('Got no meta columns to remove from the data set. ')
+  }
+
+  if (is.null(phenoData) & is.null(phenoFile)) {
+    stop('Got no phenotype dataset or file to read the phenotype data from. ')
+  }
+
+  if (is.null(phenoData) & !is.null(phenoFile)) {
+    phenoData <- data.frame(fread(phenoFile, sep="\t", na.strings = c("NA", " ", "--", "-", ".", "..")))
+  }
+
+  if(!is.null(metaDataFile)) {
+    metaData <- scan(metadataFile, what="character")
+  }
+
+  allNames <- names(phenoData)
+
+  allTraitNames <- allNames[! allNames %in% nonTraitNames]
+  selectCols <- c(keepMetaCols, allTraitNames)
+
+  phenoData <- phenoData %>% select_(selectCols)
+
+}
