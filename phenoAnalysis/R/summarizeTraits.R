acafb4e3aad8682242fbfcc91a66bde5e16832e6
Isaak <iyt2@cornell.edu>
2019-01-21 10:15:58 +0100

    add optional traits argument

--- phenoAnalysis/R/summarizeTraits.R
+++ phenoAnalysis/R/summarizeTraits.R
@@ -3,19 +3,28 @@
 #' Groups by given factor and calculates summary data
 #'
 #' @param phenoData a data frame of the phenotype dataset
+#' @param traits a character vector of traits summary to calculate for.
+#'               If not given, summary will be calculated for all traits in the data set.
 #' @param groupBy a vector of the columns to group by, defaults to germplasmName.
 #' @param summaryStat a vector of the summary statistics to calculate, defaults to mean.
 #' @return a data frame of the phenotype dataset with summarized by the grouping factors.
 #' @export
 #'
-summarizeTraits <- function(phenoData=NULL, groupBy=c('germplasmName'), summaryStat=c('mean')) {
+summarizeTraits <- function(phenoData=NULL,
+                            traits=NULL,
+                            groupBy=c('germplasmName'),
+                            summaryStat=c('mean')) {
 
   if (is.null(phenoData)) {
   stop('No dataset given. ')
   }
 
   allCols <- names(phenoData)
-  traitCols <- allCols[! allCols %in% groupBy]
+  if (!is.null(traits)){
+    traitCols <- traits
+  } else {
+    traitCols <- allCols[! allCols %in% groupBy]
+  }
 
   summaryData <- phenoData %>%
                  group_by_(.dots=groupBy) %>%
