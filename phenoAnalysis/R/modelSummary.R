89280e3f66a8636f6960dd73ddcce2a57e1c57fd
Isaak <iyt2@cornell.edu>
2019-04-25 09:09:19 +0200

    comment out duplicate function

--- phenoAnalysis/R/modelSummary.R
+++ phenoAnalysis/R/modelSummary.R
@@ -1,63 +1,63 @@
-#' fits model and returns the model output summary
-#'
-#' fits model and returns the model output summary
-#' @param trialData a dataframe of the trial dataset, of the BrAPI structure and naming convention.
-#' @param traitName a character vector of the trait name of interest in the dataset.
-#' @param genotypeEffectType a character vector indicating the genotype effect type. By default, genotypes variable is considered fixed effects.
-#' @return a summary of the model output.
-#' @export
-#'
+## #' fits model and returns the model output summary
+## #'
+## #' fits model and returns the model output summary
+## #' @param trialData a dataframe of the trial dataset, of the BrAPI structure and naming convention.
+## #' @param traitName a character vector of the trait name of interest in the dataset.
+## #' @param genotypeEffectType a character vector indicating the genotype effect type. By default, genotypes variable is considered fixed effects.
+## #' @return a summary of the model output.
+## #' @export
+## #'
 
-runAnova <- function (trialData, traitName=NULL, genotypeEffectType='fixed') {
+## runAnova <- function (trialData, traitName=NULL, genotypeEffectType='fixed') {
 
-  if (is.null(traitName)) stop('Trait name is missing.')
+##   if (is.null(traitName)) stop('Trait name is missing.')
 
-  tr <- grep(traitName, colnames(trialData))
-  if (length(tr) == 0) {
-    stop('Can not find ', traitName, ' in the data set.')
-  }
+##   tr <- grep(traitName, colnames(trialData))
+##   if (length(tr) == 0) {
+##     stop('Can not find ', traitName, ' in the data set.')
+##   }
 
-  genotypeEffectType <<- genotypeEffectType
-  traitData          <- structureTraitData(trialData, traitName=traitName)
-  anovaTable         <- c()
+##   genotypeEffectType <<- genotypeEffectType
+##   traitData          <- structureTraitData(trialData, traitName=traitName)
+##   anovaTable         <- c()
 
-  studyDesign <- trialData[2, 'studyDesign']
-  if (is.na(studyDesign) == TRUE) studyDesign <- c('No Design')
+##   studyDesign <- trialData[2, 'studyDesign']
+##   if (is.na(studyDesign) == TRUE) studyDesign <- c('No Design')
 
-  if (studyDesign == 'RCBD'&&  length(unique(traitData$blockNumber)) > 1) {
-     if (genotypeEffectType == 'fixed') {
-      modelOut   <- fixedRCBD(traitData, traitName)
-    } else {
-      modelOut   <- randomRCBD(traitData, traitName)
-    }
-  } else if (studyDesign == 'Augmented' &&  length(unique(traitData$blockNumber)) > 1) {
+##   if (studyDesign == 'RCBD'&&  length(unique(traitData$blockNumber)) > 1) {
+##      if (genotypeEffectType == 'fixed') {
+##       modelOut   <- fixedRCBD(traitData, traitName)
+##     } else {
+##       modelOut   <- randomRCBD(traitData, traitName)
+##     }
+##   } else if (studyDesign == 'Augmented' &&  length(unique(traitData$blockNumber)) > 1) {
 
-    if (genotypeEffectType == 'fixed') {
-      modelOut   <- fixedAugmentedRCBD(traitData, traitName)
-    } else {
-      modelOut   <- randomAugmentedRCBD(traitData, traitName)
-    }
+##     if (genotypeEffectType == 'fixed') {
+##       modelOut   <- fixedAugmentedRCBD(traitData, traitName)
+##     } else {
+##       modelOut   <- randomAugmentedRCBD(traitData, traitName)
+##     }
 
-  }  else if (studyDesign == 'CRD' &&  length(unique(traitData$replicate)) > 1) {
+##   }  else if (studyDesign == 'CRD' &&  length(unique(traitData$replicate)) > 1) {
 
-    if (genotypeEffectType == 'fixed') {
-      modelOut   <- fixedCRD(traitData, traitName)
-    } else {
-      modelOut   <- randomCRD(traitData, traitName)
-    }
+##     if (genotypeEffectType == 'fixed') {
+##       modelOut   <- fixedCRD(traitData, traitName)
+##     } else {
+##       modelOut   <- randomCRD(traitData, traitName)
+##     }
 
-  } else if (studyDesign == 'Alpha') {
+##   } else if (studyDesign == 'Alpha') {
 
-    if (genotypeEffectType == 'fixed') {
-      modelOut  <- fixedAlpha(traitData, traitName)
-    } else {
-      modelOut   <- randomAlpha(traitData, traitName)
-    }
+##     if (genotypeEffectType == 'fixed') {
+##       modelOut  <- fixedAlpha(traitData, traitName)
+##     } else {
+##       modelOut   <- randomAlpha(traitData, traitName)
+##     }
 
-  } else {
-    modelOut <- c('This trial has no design.')
-  }
+##   } else {
+##     modelOut <- c('This trial has no design.')
+##   }
 
-  return(modelOut)
-}
+##   return(modelOut)
+## }
 
