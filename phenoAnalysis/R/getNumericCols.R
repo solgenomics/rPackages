35da37845423f80855f5358b462cbc58b9eceedc
Isaak <iyt2@cornell.edu>
2022-05-07 15:02:20 +0200

    add option to keep non-numeric cols

--- phenoAnalysis/R/getNumericCols.R
+++ phenoAnalysis/R/getNumericCols.R
@@ -1,12 +1,12 @@
 #' removes non-numeric columns from a data frame
 #'
 #' @param phenoData a data frame of the phenotype dataset
-#' @param traits a vector of trait columns to check if they are numeric or not. By default,
-#'        all columns checked.
+#' @param exception a vector of columns to skip checking if they are numeric.
+#'                By default, it skips the 'germplasmName' column.
 #' @return a data frame of the phenotype dataset with without the non-numeric variables.
 #' @export
 #'
-getNumericCols <- function(phenoData=NULL, traits=NULL) {
+getNumericCols <- function(phenoData=NULL, exclude=c('germplasmName')) {
 
   if (is.null(phenoData)) {
     stop('You have not provided a data frame of a dataset or it is an empty data frame.')
@@ -15,14 +15,11 @@ getNumericCols <- function(phenoData=NULL, traits=NULL) {
   # if (is.null(traits)) {
   #   stop('You need to provide a vector of the traits/columns to screen for numeric data types.')
   # }
+
   phenoData <- data.frame(phenoData)
-  if (is.null(traits)) {
-    traits <- colnames(phenoData)
-  }
 
   phenoData <- phenoData %>%
-               select(traits) %>%
-               select_if(is.numeric)
+               select(matches(exclude), names(select_if(., ~is.numeric(.x))))
 
   return (phenoData)
 
