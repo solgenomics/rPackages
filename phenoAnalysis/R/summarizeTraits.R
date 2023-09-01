11868a56fc42f304866107dd0476f4b114627011
Isaak <iyt2@cornell.edu>
2023-09-01 10:43:20 +0200

    update select condition

--- phenoAnalysis/R/summarizeTraits.R
+++ phenoAnalysis/R/summarizeTraits.R
@@ -44,7 +44,7 @@ summarizeTraits <- function(phenoData=NULL,
   summaryData <- phenoData %>%
                  group_by(across(all_of(groupBy))) %>%
                  summarise_at(across(all_of(traitsCols)), summaryStat, na.rm=TRUE) %>%
-                 select(which(colSums(is.na(.)) != nrow(.) ))
+                 select(where(~!all(is.na(.x))))
 
   summaryData[summaryData == 'NaN'] <- NA
 
