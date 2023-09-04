8f4e00df505b02a427838337dc11618cd257fad1
Isaak <iyt2@cornell.edu>
2023-09-04 10:12:18 +0200

    make averageTrait also calculate median

--- phenoAnalysis/R/averageTrait.R
+++ phenoAnalysis/R/averageTrait.R
@@ -5,14 +5,25 @@
 #' @param traitName a character vector or string of the trait name of interest in the dataset.
 #' @return a dataframe of genotypes mean trait values.
 #' @param meansVariable name of the variable averages to calculate for, the default is germplasmName.
+#' @param averageType character vector of type(s) of averages (mean or median) to calculate, the default is mean.
 #' @export
 #'
 
 averageTrait <- function(traitData,
                          traitName,
-                         meansVariable='germplasmName') {
+                         meansVariable='germplasmName',
+                         averageType=c('mean')) {
 
   warning('This is just arthemetic average of the trait.')
+
+  traitAverageHeader <- c()
+  if(averageType == 'mean') {
+    traitAverageHeader <- paste0(traitName, '_Arithmetic_mean')
+  } else if (averageType == 'median') {
+    traitAverageHeader<- paste0(traitName, '_median')
+  } else {
+    stop(paste0("Error occured calculating trait average. Can't calculate ", averageType, "."))
+  }
 
   traitData <- traitData[, c(meansVariable, traitName)]
   traitData[, traitName] <- as.numeric(as.character(traitData[, traitName]))
@@ -24,16 +35,12 @@ averageTrait <- function(traitData,
   traitData   <- traitData[order(row.names(traitData)), ]
   traitData   <- data.frame(traitData)
 
-
-  #calMean <- paste0('mean(', traitName, ', na.rm=TRUE)')
-  #aveCol  <- traitName
-
   traitAverage <- traitData %>%
     group_by(across(all_of(meansVariable))) %>%
-    summarise(traitName = mean({{traitName}}))
+    summarise_at(traitName, averageType, na.rm = TRUE)
 
   traitAverage <- data.frame(traitAverage)
-  names(traitAverage)[2] <- paste0(traitName, '_Arithmetic_mean')
+  names(traitAverage)[2] <- traitAverageHeader
 
   return(traitAverage)
 
